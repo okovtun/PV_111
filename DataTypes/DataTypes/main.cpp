7094d5668ea0b07c817bc685e09c5305cbce198f
Clayman <Clayman@DESKTOP-DM8E77U>
2022-02-18 19:11:27 +0200

    DataTypes start

---
+++ DataTypes/DataTypes/main.cpp
@@ -0,0 +1,13 @@
+//DataTypes
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	//endl (endline) - переход в начало новой строки, так же как "\n"
+	cout << true << "\n";
+	cout << false << endl;
+	//false - это 0
+	//true - все что НЕ 0
+}
