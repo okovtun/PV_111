13917eefeab3f59da45f313f139b93b463cb2bb2
Clayman <Clayman@DESKTOP-DM8E77U>
2022-02-22 21:14:27 +0200

    DataTypes day 2

--- DataTypes/DataTypes/main.cpp
+++ DataTypes/DataTypes/main.cpp
@@ -2,12 +2,46 @@
 #include<iostream>
 using namespace std;
 
+//#define BOOLEAN
+//#define INTEGRAL_TYPES
+
 void main()
 {
 	setlocale(LC_ALL, "Russian");
+#if defined BOOLEAN
 	//endl (endline) - переход в начало новой строки, так же как "\n"
 	cout << true << "\n";
 	cout << false << endl;
 	//false - это 0
 	//true - все что Ќ≈ 0
+#endif
+
+#if defined INTEGRAL_TYPES
+	//sizeof(type or value)
+	cout << "short:\n";
+	cout << sizeof(short) << endl;
+	cout << SHRT_MIN << "..." << SHRT_MAX << endl;
+	cout << 0 << "..." << USHRT_MAX << endl;
+#endif
+
+	//float		- дробное число одинарной точности	4 Bytes
+	//double	- дробное число двойной точности	8 Bytes
+	cout << "float:\n";
+	cout << sizeof(float) << endl;
+	cout << FLT_MIN << " ... " << FLT_MAX << endl;
+	cout << endl;
+
+	cout << "double:\n";
+	cout << sizeof(double) << endl;
+	cout << DBL_MIN << " ... " << DBL_MAX << endl;
+	cout << endl;
+
+	int a;
+	double price;
+	double price_of_coffee;
+	//проинициализировать
+	//инициализаци€ - это присвоение начального значени€.
+	cout << "¬ведите стоимость кофе: ";
+	cin >> price_of_coffee;
+	//cin - Console Input, поток ввода с клавиатуры
 }
---
+++ DataTypes/HomeWork/main.cpp
@@ -0,0 +1,50 @@
+﻿#include<iostream>
+using namespace std;
+
+#define offset "\t\t\t\t\t"
+//offset - макроопределение (макрос), а точнее, имя макроса
+//"\t\t\t\t\t" - значение макроса
+//#define MACROS
+#if defined MACROS
+	//code
+	//Если MACROS определен выше, то этот код будет виден компилятору,
+	//в противном случае, этот код будет проигноорирован компилятором.
+#endif
+
+//#define ELOCHKA
+//define	 - определить
+//definition - определение
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+
+#if defined ELOCHKA
+//if - если
+//defined - определено
+	cout << offset << "В лесу родилась ёлочка,\n";
+	cout << offset << "В лесу она росла,\n";
+	cout << offset << "Зимой и летом стройная,\n";
+	cout << offset << "Зелёная была.\n";
+	cout << "\n";
+	cout << offset << "Метель ей пела песенку:\n";
+	cout << offset << "Спи, елочка, бай - бай!\n";
+	cout << offset << "Мороз снежком укутывал:\n";
+	cout << offset << "Смотри, не замерзай!\n";
+	cout << offset << "Мороз снежком укутывал:\n";
+	cout << offset << "Смотри, не замерзай!\n";
+	cout << "\n";
+	cout << offset << "Трусишка зайка серенький\n";
+	cout << offset << "Под ёлочкой скакал.\n";
+	cout << offset << "Порою волк, сердитый волк,\n";
+	cout << offset << "Рысцою пробегал.\n";
+	cout << offset << "Порою волк, сердитый волк,\n";
+	cout << offset << "Рысцою пробегал.\n";
+	cout << "\n";
+	cout << offset << "Снег по лесу частому\n";
+	cout << offset << "Под полозом скрипит.\n";
+	cout << offset << "Лошадка мохноногая\n";
+	cout << offset << "Торопится, бежит.\n";
+#endif
+
+}
