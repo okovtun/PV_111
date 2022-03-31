07ce54fc9157b9b787e792b48a10116581d76f32
Clayman <Clayman@DESKTOP-DM8E77U>
2022-03-31 21:30:10 +0300

    Hardcore

---
+++ Arrays/Dublicates/main.cpp
@@ -0,0 +1,71 @@
+﻿//Dublicates
+#include<iostream>
+using namespace std;
+
+#define tab "\t"
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n] = {};
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % n;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		bool already_there = false;	//предполагаем что выбранный элемент ранее НЕ встречался,
+		//но это нужно проветрить:
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[i] == arr[j])
+			{
+				already_there = true;	//этот элемент уже встречался ранее
+				break;
+			}
+		}
+
+		if (already_there)continue;//Ключевое слово 'continue' 
+		//прерывает текущую итерацию, и переходт к следующей
+
+		int count = 1;	//счетчик вхождений
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[i] == arr[j])
+			{
+				count++;
+			}
+		}
+		if (count > 1)printf("Значение %d встречается %d раз\n", arr[i], count);
+		//https://cplusplus.com/reference/cstdio/printf/?kw=printf
+		//if (count > 1)cout << arr[i] << " встречается " << count << " раз" << endl;
+		//if (count > 0)cout << arr[i] << " повторяется " << count << " раз" << endl;
+
+#ifdef SEARCH_1
+		if (!already_there)
+		{
+			int count = 1;	//счетчик вхождений
+			for (int j = i + 1; j < n; j++)
+			{
+				if (arr[i] == arr[j])
+				{
+					count++;
+				}
+			}
+			if (count > 1)cout << arr[i] << " встречается " << count << " раз" << endl;
+			//if (count > 0)cout << arr[i] << " повторяется " << count << " раз" << endl;
+		}
+#endif // SEARCH_1
+
+
+
+	}
+}
--- Arrays/UniqueRand/main.cpp
+++ Arrays/UniqueRand/main.cpp
@@ -9,10 +9,40 @@ void main()
 	setlocale(LC_ALL, "");
 	const int n = 10;
 	int arr[n] = {};
+	bool unique;
+#ifdef UNIQUE_1
+	for (int i = 0; i < n; i++)
+	{
+		do
+		{
+			arr[i] = rand() % n;
+			unique = true;	//Предполагаем, что сгенерированное случайное число уникально,
+			//но это нужно проверить:
+			for (int j = 0; j < i; j++)
+			{
+				if (arr[i] == arr[j])
+				{
+					unique = false;
+					break;//прерывает текущую итерацию, и все последующие
+				}
+			}
+		} while (!unique);
+	}
+#endif // UNIQUE_1
+
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % n;
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[i] == arr[j])
+			{
+				i--;
+				break;
+			}
+		}
 	}
+
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << tab;
