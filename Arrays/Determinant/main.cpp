fb3ee399327874f866807669538ba73d95bdc70d
Oleg <31523835+okovtun@users.noreply.github.com>
2022-04-05 21:42:16 +0300

    2D arrays day 2

--- Arrays/Determinant/main.cpp
+++ Arrays/Determinant/main.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
 
 #define tab "\t"
@@ -6,5 +6,75 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
+	const int n = 3;	//количество элементов строки
+	int A[n][n] =
+	{
+		{1,2,3},
+		{4,5,6},
+		{7,8,9},
+	};
+	/*for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			A[i][j] = rand() % 10;
+		}
+	}*/
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << A[i][j] << tab;
+		}
+		cout << endl;
+	}
 
+	//Создаем расширенную матрицу:
+	int B[n][n * 2] = {};
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			B[i][j] = B[i][j + n] = A[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n * 2; j++)
+		{
+			cout << B[i][j] << tab;
+		}
+		cout << endl;
+	}
+
+	//////////////////////////////////////////////////////
+	////////		Вычисляем определитель:		 /////////
+	//////////////////////////////////////////////////////
+
+	//Вычисляем главную диагональ:
+	int main_diag = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int product = 1;	//произведение элементов главной диагонали
+		for (int j = 0; j < n; j++)
+		{
+			product *= B[j][i + j];
+		}
+		main_diag += product;
+	}
+	cout << "Главная диагональ: " << main_diag << endl;
+
+	//Вычисляем вспомогательную диагональ:
+	int aux_diag = 0;	//aux - Auxilliary (Вспомогательный)
+	for (int i = 0; i < n; i++)
+	{
+		int product = 1;
+		for (int j = 0; j < n; j++)
+		{
+			product *= B[n - 1 - j][i + j];
+		}
+		aux_diag += product;
+	}
+	cout << "Вспомогательная диагональ: " << aux_diag << endl;
+	cout << "Определитель матрицы: " << main_diag - aux_diag << endl;
 }
---
+++ Arrays/Sort2/main.cpp
@@ -0,0 +1,64 @@
+//Sort2
+#include<iostream>
+using namespace std;
+
+#define tab "\t"
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int ROWS = 8;
+	const int COLS = 5;
+	int arr[ROWS][COLS] = {};
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+	//Вывод исходного массива на экран:
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+
+	//Сортировка массива:
+	int iterations = 0;
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			for (int k = i; k < ROWS; k++)
+			{
+				//		  condition ? value1 : value2;
+				for (int l = k == i ? j + 1 : 0; l < COLS; l++)
+				{	
+					if (arr[k][l] < arr[i][j])
+					{
+						int buffer = arr[i][j];
+						arr[i][j] = arr[k][l];
+						arr[k][l] = buffer;
+					}
+					iterations++;
+				}
+			}
+		}
+	}
+	cout << "Массив отсортирован за " << iterations << " итераций\n";
+	cout << "Отсорртированный массив:\n";
+	//Вывод отсортированного массива на экран:
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+
+}
